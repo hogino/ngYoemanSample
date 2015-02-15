88b90f5cac2b88d1538a3e74661c711144daf03e
hogino <hogino1003@gmail.com>
2015-02-15 17:41:15 +0900

    tutorial step10

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('ngJhacSampleApp'));
+  beforeEach(module('ngYoemanSampleApp'));
 
   var AboutCtrl,
     scope;
